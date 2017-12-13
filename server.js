e717318314859cc9b28829cc5789280d40342a70
KevinPham <kevinpham15@gmail.com>
2017-12-12 17:10:23 -0800

    notes/styles

--- server.js
+++ server.js
@@ -14,17 +14,24 @@ var bodyParser = require("body-parser");
 // ============== Second, invokes the express app ===========//
 var app = express();
 
+
+
+
 // ============== use static files?? ===========//
 app.use(express.static(__dirname + "/static"));
 
-// ============== use style.css ===========//
+// ============== use STYLE.CSS ===========//
 app.use(express.static(path.join(__dirname, 'static')));
+
+
 
 // ============== Setting up ejs and our views folder ===========//
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
     // this piece of code needs to be here!!!
+
+
 
 
 
